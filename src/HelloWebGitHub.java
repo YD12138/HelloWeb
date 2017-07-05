306ea7c997d7756b31f497d1a55e7825790200ca
YD12138 <1571568193@qq.com>
2017-07-05 09:06:19 +0800

    Add

---
+++ src/HelloWebGitHub.java
@@ -0,0 +1,10 @@
+
+public class HelloWebGitHub {
+	public static void main(String[] args){
+		System.out.println("Hello Web for GitHub!!!!");
+		
+		//Add from local
+		System.out.println("Add from local");
+	}
+
+}
